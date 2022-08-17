01a484c6aef6115c9b27b4b6c89fcb15afbd6284
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2022-08-17 18:53:41 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (30.18%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,9 +1,6 @@
 class Solution {
     public void sortColors(int[] num) {
-        // your logic can not use extra space it is a mean of in place sorting
-        //visiting all the value sequentially is c/d traversal
-        // when two pass solution convert in one pass solution itm is cld three poinatapproach
-       int n=num.length;
+        int n=num.length;
         int low=0,mid=0,high=n-1;
         while(mid<=high)
         {
