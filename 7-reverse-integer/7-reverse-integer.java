faa6d945a30a6aef57dc13cc0dbeaf5efa3b6862
Isha Agarwal <79989964+ishaagrawal67@users.noreply.github.com>
2022-08-17 08:37:11 +0530

    Time: 2 ms (78.43%), Space: 42.1 MB (11.30%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int reverse(int x) {
-       long rev=0;
+        long rev=0;
         while(x!=0)
         {
             rev=rev*10+(x%10);
@@ -11,5 +11,6 @@ class Solution {
             return 0;
         }
         return (int )rev;
+        
     }
 }
